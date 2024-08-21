ec6babcfc1dda992d457f5ed30f5f1410cc6110c
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2024-08-21 09:14:22 -0500

    housekeeping(swagger): remove swagger config

--- src/controllers/routes/carts/index.ts
+++ src/controllers/routes/carts/index.ts
@@ -9,30 +9,6 @@ import {
 
 const carts = Router();
 
-/**
- * @swagger
- * /carts:
- *   get:
- *     summary: Retrieve a list of carts
- *     responses:
- *       200:
- *         description: A list of carts.
- *         content:
- *           application/json:
- *             schema:
- *               type: array
- *               items:
- *                 type: object
- *                 properties:
- *                   id:
- *                     type: integer
- *                     example: 1
- *                   items:
- *                     type: array
- *                     items:
- *                       type: string
- *                     example: ['item1', 'item2']
- */
 carts.get("/", getCartsController);
 carts.get("/:id", getCartByIdController);
 carts.put("/:id", updateCartByIdController);
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,8 @@
 import "dotenv/config";
 
 import app from "./controllers/app";
-import { setupSwagger } from "./controllers/routes/docs/swagger";
 
 const port = process.env.APP_PORT || 3000;
-
-setupSwagger(app);
 
 app.listen(port, () => {
   console.log("INSTACART LISTENING ON PORT", port);
